e944b7b813aa2cfc7e4587260aa1441db83ed133
GDECUIR25 <51933349+GDECUIR25@users.noreply.github.com>
2021-11-22 17:03:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,33 +10,72 @@
     ?>
 </head>
 <body>
+	<?php
+	$display = false;
+	?>
     <form action="#" method="POST">
         <label>Enter Symbol/Name:</label>
-        <input type="text" id='coininput' name='inText'></input>
-        <button type=sumbit name=submit id='Submit'> Submit</button>
+        <input type="text" id='coininput' name='userText'></input>
+        <button type=sumbit name=submit id="Submit"> Submit</button>
     </form>
     <?php 
-    gettopthousand();
+	gettopthousand();
     if (isset($_POST['submit'])){
-        try{(query("name",$_POST['inText']));
-            // echo("Found Name");
+        try{(query("name",$_POST['userText']));
+            $display = true;
         }catch(Exception $e){
-            try{(query("symbol",$_POST['inText']));
-                // echo("Found Symbol");
+            try{(query("symbol",$_POST['userText']));
+                $display = true;
             }catch(Exception $e){
-                try{(APICall("slug",strtolower($_POST['inText'])));
-                }catch(Exception $e){
-                    try{(APICall("symbol",$_POST['inText']));
-                    }catch(Exception $e){
-                        echo("Coin Not Found");
-                }
+                $display = false;
+                echo($_POST['userText'] . " not found or not within top 1000 coins.");
             }
         }
-    }
-}	
-    topTenPrint();
+    }	
     ?>
+	<?php if($display) : ?>
+		<table id="resultdisplay">
+			<tr>
+			<td></td>
+			<td></td>
+			<th>Importance</th>
+			</tr>
+			<tr>
+				<th>Market Cap</th>
+				<td id="MarketCapResult"></td>
+				<td><input class=slider type="range" min="0" max="100" name="sld2" value="100"></td>
+			</tr>
+			<tr>
+				<th>Volume(24hr)</th>
+				<td id="VolumeResult"></td>
+				<td><input class=slider type="range" min="0" max="100" name="sld3" value="100"></td>
+			</tr>
+			<tr>
+				<th>Change 1hr</th>
+				<td id="Change1Result"></td>
+				<td><input class=slider type="range" min="0" max="100" name="sld3" value="100"></td>
+			</tr>
+			<tr>
+				<th>Change 24hr</th>
+				<td id="Change24Result"></td>
+				<td><input class=slider type="range" min="0" max="100" name="sld4" value="100"></td>
+			</tr>
+			<tr>
+				<th>Change 7d</th>
+				<td id="Change7dResult"></td>
+				<td><input class=slider type="range" min="0" max="100" name="sld5" value="100"></td>
+			</tr>
+		</table>
+		
+		<button type="button">Calculate</button>
+		
+		<a href="http://localhost/BigLeaf/">
+		<button>New Search</button>
+		</a>
+	<?php else : topTenPrint(); ?>
+	
+	<?php endif; ?>
 
-    
+    </table>	
 </body>
 </html>
